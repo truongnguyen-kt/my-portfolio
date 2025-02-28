0dff5035cef6410d21a13d51684843eed015eef8
truongnguyen-kt <ngtruong166@gmail.com>
2025-02-28 15:49:34 +0700

    init backend

---
+++ portfolio-backend/src/test/java/com/blues/portfoliobackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blues.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
